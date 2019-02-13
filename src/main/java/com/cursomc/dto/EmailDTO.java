3ebb12b8523f65fcdbb54a32d59d16b4fb425d95
luizdragneel <luiz.filipe.vieira@hotmail.com>
2019-02-13 17:03:23 -0200

    Esqueci a senha

---
+++ src/main/java/com/cursomc/dto/EmailDTO.java
@@ -0,0 +1,28 @@
+package com.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class EmailDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email invalido")
+	private String email;
+	
+	public EmailDTO() {
+		
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
--- src/main/java/com/cursomc/services/EmailService.java
+++ src/main/java/com/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void  sendNewPasswordEmail(Cliente cliente, String newPass);
 }
